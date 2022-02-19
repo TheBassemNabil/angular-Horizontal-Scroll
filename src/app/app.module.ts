32891d5ca1869a265520c4c5373fdd398affacc7
TheBassemNabil <bass.mar111@outlook.com>
2022-02-19 10:52:55 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-// import { NgHorizontalScrollModule } from 'angular-horizontal-scroll-table';
+import { NgHorizontalScrollModule } from 'angular-horizontal-scroll-table';
 import { HorizontalScrollComponent } from './horizontalScroll/horizontalScroll.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,HttpClientModule ,
-    //  NgHorizontalScrollModule
-     ],
-  declarations: [ AppComponent, HorizontalScrollComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgHorizontalScrollModule,
+  ],
+  declarations: [AppComponent, HorizontalScrollComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
